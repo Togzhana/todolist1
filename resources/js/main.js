799917981beea13916def805d7b6b32909509216
Togzhan <togzhan.kurmanbek@mail.ru>
2020-05-06 13:54:41 +0600

    delete and complete icons

---
+++ resources/js/main.js
@@ -0,0 +1,12 @@
+
+//user clicked on the add button
+//is there any text field in item field, add that text to the todo list
+document.getElementById('add').addEventListener('click', function(){
+
+	var value = document.getElementById('item').value;
+
+	if(value){
+		
+	}
+	console.log(value);
+});
